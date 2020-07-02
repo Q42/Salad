b3493a9ddffe30fa54ad56925c97543b7138e1b7
Mathijs Kadijk <mkadijk@gmail.com>
2020-07-03 00:28:26 +0200

    Add non polling checks before waiter
    - Check the predicate without using `XCTWaiter` once so we don’t have the polling delay
    - If the `TimeOut` is set to `instant`, don’t initialize the waiter but fail immediatly
    - Removed `waitForLabelNotNill` since the label can’t be nil according to the Swift code and it’s not used anywhere?!

--- Sources/Salad/Extensions/XCUIElement+Extensions.swift
+++ Sources/Salad/Extensions/XCUIElement+Extensions.swift
@@ -10,26 +10,28 @@ import XCTest
 
 public extension XCUIElement {
   func waitForExist(timeout: TimeOut) -> Bool {
-    waitForExistence(timeout: timeout.timeInterval)
+    if exists { return true }
+    return waitForExistence(timeout: timeout.timeInterval)
   }
 
   func waitForExistsAndHittable(timeout: TimeOut) -> Bool {
-    waitForPredicate(NSPredicate(format: "exists == true && hittable == true"), timeout: timeout)
+    if exists && isHittable { return true }
+    return waitForPredicate(NSPredicate(format: "exists == true && hittable == true"), timeout: timeout)
   }
 
   func waitForNotExists(timeout: TimeOut) -> Bool {
-    waitForPredicate(NSPredicate(format: "exists == false"), timeout: timeout)
-  }
-
-  func waitForLabelNotNill(timeout: TimeOut) -> Bool {
-    waitForPredicate(NSPredicate(format: "label != nil"), timeout: timeout)
+    if !exists { return true }
+    return waitForPredicate(NSPredicate(format: "exists == false"), timeout: timeout)
   }
 
   func waitForLabel(toBecome value: String, timeout: TimeOut) -> Bool {
-    waitForPredicate(NSPredicate(format: "label == %@", value), timeout: timeout)
+    if label == value { return true }
+    return waitForPredicate(NSPredicate(format: "label == %@", value), timeout: timeout)
   }
 
   private func waitForPredicate(_ predicate: NSPredicate, timeout: TimeOut) -> Bool {
+    if case .instant = timeout { return false }
+
     let expectation = XCTNSPredicateExpectation(predicate: predicate, object: self)
     return XCTWaiter.wait(for: [expectation], timeout: timeout.timeInterval) == .completed
   }
