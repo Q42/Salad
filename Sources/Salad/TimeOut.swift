961316920c1e906dcb5dea4bf76001f480f316b1
Mathijs Kadijk <mkadijk@gmail.com>
2020-07-03 01:28:24 +0200

    Convert TimeOut enum to a struct
    Seems cleaner now it’s just convenience around TimeInterval.

--- Sources/Salad/TimeOut.swift
+++ Sources/Salad/TimeOut.swift
@@ -8,29 +8,36 @@
 
 import Foundation
 
-public enum TimeOut {
+/// Represents the length of the time out when waiting for something during your UI test to (dis)appear or finish.
+///
+/// Offers some sane default values to use, but is also easy to initialize with your own custom `TimeInterval`.
+public struct TimeOut: ExpressibleByFloatLiteral, ExpressibleByIntegerLiteral, Equatable {
+  public typealias IntegerLiteralType = TimeInterval
+  public typealias FloatLiteralType = TimeInterval
+
   /// Directly time out, do not wait at all
-  case instant
+  public static let instant: TimeOut = 0
 
-  /// Time out for a regular network call that shortly blocks user interaction
-  case regularNetworkCall
+  /// Appropiate time out for UI that is not rendered instantly, but waits for an animation completion or is dispatched to the next render loop for example
+  public static let asyncUI: TimeOut = 1
 
-  /// Time that it should take to switch to another app
-  case appSwitch
+  /// Appropiate time out for a fast network call that blocks user interaction, but is usually completed within a second
+  public static let fastNetworkCall: TimeOut = 10
 
-  /// There are cases where the ui takes a tiny bit of time to continue, sadly..
-  case asyncUI
+  /// Appropiate time out for launching and switching to another app, such as opening Safari from your app
+  public static let appSwitch: TimeOut = 10
 
-  /// Time out for the login network call that blocks user interaction
-  case longNetworkCall
+  /// Appropiate time out for a fast network call that blocks user interaction and is sometimes taking a few seconds
+  public static let slowNetworkCall: TimeOut = 30
 
-  public var timeInterval: TimeInterval {
-    switch self {
-    case .instant: return 0
-    case .regularNetworkCall: return 10
-    case .longNetworkCall: return 20
-    case .appSwitch: return 10
-    case .asyncUI: return 1
-    }
+  /// The time interval of this time out
+  public let timeInterval: TimeInterval
+
+  public init(floatLiteral value: TimeInterval) {
+    self.timeInterval = value
+  }
+
+  public init(integerLiteral value: TimeInterval) {
+    self.timeInterval = value
   }
 }
